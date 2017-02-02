dd92ab889dee03946e5ebc2229f544a6294cf50a
Mike Cochran <mcochran@linux.com>
2017-02-02 16:47:57 -0600

    Correct the time range

--- meetup/meetup.go
+++ meetup/meetup.go
@@ -49,7 +49,7 @@ func FilterEventsByKeyword(keyword string) (results []Event) {
 
 func GetEventsForMonth(month int, year int) (results []Event) {
 	t := time.Date(year, time.Month(month), 1, 0, 0,0,0, timezone)
-	timeframe := fmt.Sprintf("%d,1m", t.Unix())
+	timeframe := fmt.Sprintf("%d,%d", t.Unix(), t.AddDate(0,1,0).Unix())
 	r, found := c.Get(timeframe)
 	if !found {
 		log.Println("Cache miss for [", timeframe, "]")
@@ -67,7 +67,7 @@ func GetEventsForMonth(month int, year int) (results []Event) {
 
 func GetEventsForWeek(day int, month int, year int) ([]Event) {
 	t := time.Date(year, time.Month(month), day, 0, 0,0,0, timezone)
-	timeframe := fmt.Sprintf("%d,1w", t.Unix())
+	timeframe := fmt.Sprintf("%d,%d", t.Unix(), t.AddDate(0,0,7).Unix())
 	r, found := c.Get(timeframe)
 	if !found {
 		log.Println("Cache miss for [", timeframe, "]")
@@ -85,7 +85,7 @@ func GetEventsForWeek(day int, month int, year int) ([]Event) {
 
 func GetEventsForDay(day int, month int, year int) ([]Event) {
 	t := time.Date(year, time.Month(month), day, 0, 0,0,0, timezone)
-	timeframe := fmt.Sprintf("%d,1d", t.Unix())
+	timeframe := fmt.Sprintf("%d,%d", t.Unix(), t.AddDate(0,0,1).Unix())
 	r, found := c.Get(timeframe)
 	if !found {
 		log.Println("Cache miss for [", timeframe, "]")
